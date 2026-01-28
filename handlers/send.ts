10eedd48055dd03be17d2e19717c4b87228097e8
Glitchy <me@amcalledglitchy.dev>
2026-01-28 22:17:40 +0000

    made MSG packets actually respect client subscriptions - :3c

--- handlers/send.ts
+++ handlers/send.ts
@@ -1,15 +1,58 @@
-import { randomUUID } from "node:crypto";
-import { ChannelMap, Client, ClientMap } from "../models.ts";
+import { randomUUID, UUID } from "node:crypto";
+import {
+  ChannelMap,
+  Client,
+  ClientMap,
+  MSG_BROADCAST_TYPE,
+  SUBSTATE,
+} from "../models.ts";
 import {
   MsgPacket,
   normalizeUUID,
   OkPacket,
   Opcode,
+  PartialUser,
   profileToPartialUser,
   SendPacket,
   serializePacket,
 } from "../protocol.ts";
 import { ensurePacketHasNonce, errorBadState, errorNotFound } from "./utils.ts";
+
+function whatShouldIBroadcast(
+  client: Client,
+  channel: UUID,
+  mentions: PartialUser[],
+): MSG_BROADCAST_TYPE {
+  const mentioned = client.profile
+    ? mentions.some((m) => m.id === client.profile?.id)
+    : false;
+
+  const sub = client.subscriptions.get(channel) ?? client.default_sub;
+
+  switch (sub) {
+    case SUBSTATE.NONE:
+      return MSG_BROADCAST_TYPE.NONE;
+
+    case SUBSTATE.IFMENTION:
+      return mentioned ? MSG_BROADCAST_TYPE.FULL : MSG_BROADCAST_TYPE.NONE;
+
+    case SUBSTATE.PARTIAL:
+      return MSG_BROADCAST_TYPE.PARTIAL;
+
+    case SUBSTATE.PARTIALIFMENTION:
+      return mentioned ? MSG_BROADCAST_TYPE.PARTIAL : MSG_BROADCAST_TYPE.NONE;
+
+    case SUBSTATE.FULLIFMENTION:
+      return mentioned ? MSG_BROADCAST_TYPE.FULL : MSG_BROADCAST_TYPE.PARTIAL;
+
+    case SUBSTATE.FULL:
+      return MSG_BROADCAST_TYPE.FULL;
+
+    default:
+      console.error("the fuck", client, channel, mentions);
+      return MSG_BROADCAST_TYPE.FULL;
+  }
+}
 
 export function handleSendPacket(
   packet: SendPacket,
@@ -46,10 +89,33 @@ export function handleSendPacket(
       mentions: [],
     },
   };
+  const broadcastMessagePartial = {
+    ...broadcastMessage,
+    data: { ...broadcastMessage.data, content: undefined },
+  };
+
   const broadcastMessageSerialized = serializePacket(broadcastMessage); // avoid recalculating
+  const broadcastMessagePartialSerialized = serializePacket(
+    broadcastMessagePartial,
+  );
 
   clients.forEach((iter_client) => {
-    iter_client.socket.send(broadcastMessageSerialized); // no good dumb test logic
+    switch (
+      whatShouldIBroadcast(
+        iter_client,
+        targetChannel as UUID,
+        broadcastMessage.data.mentions,
+      )
+    ) {
+      case MSG_BROADCAST_TYPE.FULL:
+        iter_client.socket.send(broadcastMessageSerialized);
+        break;
+      case MSG_BROADCAST_TYPE.PARTIAL:
+        iter_client.socket.send(broadcastMessagePartialSerialized);
+        break;
+      case MSG_BROADCAST_TYPE.NONE:
+        break;
+    }
   });
 
   const okPacket: OkPacket = {
